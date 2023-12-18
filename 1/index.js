6ca933e1c193a4440ea80f1c5e546df95a3248af
Jakub <benqumichalik3@gmail.com>
2023-12-18 21:43:52 +0100

    finish part 1

--- 1/index.js
+++ 1/index.js
@@ -1,27 +1,23 @@
-const file = Bun.file('./test.txt');
-// const file = Bun.file('./input.txt');
+// const file = Bun.file('./test.txt');
+const file = Bun.file('./input.txt');
 const text = await file.text();
 
 const output = (input) => {
   let sum = 0;
-  const words = input.split('\n').map((i) => {
+  input.split('\n').map((i) => {
     let a = '';
     for (const j of i) {
       if (isNaN(parseInt(j))) continue;
       else {
-        // get first number
-        // get last number
         a = a.concat(j);
-        if (j === a.slice(-1)) {
-          if (a.length === 1) {
-            console.log(a);
-            a = a.concat(a);
-            sum += parseInt(a);
-          } else sum += parseInt(a);
-        }
       }
     }
+    if (a.length) {
+      a = a[0] + a.charAt(a.length - 1);
+      sum += parseInt(a);
+    }
   });
+
   return sum;
 };
 
