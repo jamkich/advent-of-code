0689d723f0b5450ab2a4e141543496c172c786f3
Jakub <benqumichalik3@gmail.com>
2023-12-19 00:03:29 +0100

    finish* part 2 (solution not work on every test)

--- 1/index.js
+++ 1/index.js
@@ -1,10 +1,49 @@
-// const file = Bun.file('./test.txt');
-const file = Bun.file('./input.txt');
+'use strict';
+
+const file = Bun.file('./test3.txt');
+// const file = Bun.file('./input2.txt');
 const text = await file.text();
+
+function convertDigits(input) {
+  const num = {
+    one: 1,
+    two: 2,
+    three: 3,
+    four: 4,
+    five: 5,
+    six: 6,
+    seven: 7,
+    eight: 8,
+    nine: 9,
+  };
+
+  const regex = new RegExp(Object.keys(num).join('|'), 'ig');
+
+  const arr = input.split('\n').map((e) => {
+    let replacedWord = e;
+
+    replacedWord = replacedWord.replace(
+      regex,
+      (match) => num[match.toLowerCase()] || match,
+    );
+
+    // Replace from right to left
+    replacedWord = replacedWord.split('').reverse().join('');
+    replacedWord = replacedWord.replace(
+      regex,
+      (match) => num[match.toLowerCase()] || match,
+    );
+    replacedWord = replacedWord.split('').reverse().join('');
+
+    return replacedWord;
+  });
+
+  return arr;
+}
 
 const output = (input) => {
   let sum = 0;
-  input.split('\n').map((i) => {
+  input.map((i) => {
     let a = '';
     for (const j of i) {
       if (isNaN(parseInt(j))) continue;
@@ -15,10 +54,13 @@ const output = (input) => {
     if (a.length) {
       a = a[0] + a.charAt(a.length - 1);
       sum += parseInt(a);
+      console.log(a);
     }
   });
 
   return sum;
 };
 
-console.log(output(text));
+const task2 = convertDigits(text);
+console.log('-------------------');
+console.log(output(task2));
