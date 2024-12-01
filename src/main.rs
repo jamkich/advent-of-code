b778d3d1e2f9cbf6330fbe59f9e42da29ed13e52
Jakub <benqumichalik3@gmail.com>
2024-12-01 18:50:01 +0100

    setup for completing with rust

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+mod days;
+mod utils;
+fn main() {
+    let day = std::env::args()
+        .nth(1)
+        .expect("give day number as argument");
+    match day.as_str() {
+        "1" => days::day01::run(),
+        _ => eprintln!("Day {} not exist", day),
+    }
+}
